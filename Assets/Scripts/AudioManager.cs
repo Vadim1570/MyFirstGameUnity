3972a0294aab305702e20e776de045118c475020
Vadim1570 <khamzin-vadim@yandex.ru>
2021-01-11 18:59:14 +0300

    add sea sound and change chicken sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
     public Sound[] sounds;
 
     // Start is called before the first frame update
     void Awake()
     {
+        if(instance == null) instance = this; else { Destroy(gameObject); return; }
+        DontDestroyOnLoad(gameObject);
+
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
